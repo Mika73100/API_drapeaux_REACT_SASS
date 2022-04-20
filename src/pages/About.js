20658bd98182160f057d6f80ce06d0e07f709cbc
Micka73100 <Michael-73@live.Fr>
2022-04-20 09:33:17 +0200

    Api flags

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>coucou</p>
+        </div>
+    );
+};
+
+export default About;
